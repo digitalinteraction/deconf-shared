384cedcfe08ec7d3a1ed3fb61565437cc5280470
Rob Anderson <robanderson@hey.com>
2021-09-08 15:45:18 +0100

    feat: add userData to registration

--- src/registration.ts
+++ src/registration.ts
@@ -8,6 +8,7 @@ export interface RegisterRequest {
   language: string
   country: string
   affiliation: string
+  userData: unknown
 }
 
 export interface Registration {
@@ -20,4 +21,5 @@ export interface Registration {
   affiliation: string
   verified: boolean
   consented: Date
+  userData: unknown
 }
